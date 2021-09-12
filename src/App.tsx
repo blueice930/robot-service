82d762da73f9e8c1f40c02e95a3ea5b0ed7a4661
Ronnie <icatchaflyinmyhand@gmail.com>
2021-09-12 19:11:43 +0800

    💄 Add UI format

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Calculate Robot service Fee...
-        </p>
-      </header>
+      <Dashboard />
     </div>
   );
 }
